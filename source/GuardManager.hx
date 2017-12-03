7cd5fe7e884f4e8e55f32832d22dd510ee1231d9
Cody Claborn <cxsquared@gmail.com>
2017-12-02 23:48:42 -0500

    Added basic guard ai

--- source/GuardManager.hx
+++ source/GuardManager.hx
@@ -1,8 +1,11 @@
 package ;
 
+import flixel.math.FlxRandom;
+import flixel.math.FlxMath;
+import flixel.math.FlxVector;
+import flixel.math.FlxAngle;
 import flixel.math.FlxPoint;
 import flixel.FlxG;
-import flixel.tile.FlxBaseTilemap.FlxTilemapDiagonalPolicy;
 import ent.Guard;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import ent.Player;
@@ -29,25 +32,45 @@ class GuardManager {
             var curGuard:Guard = cast guard;
             var guardPoint = FlxPoint.get(curGuard.x + curGuard.width/2, curGuard.y + curGuard.height/2);
 
-            var hitPoint = new FlxPoint();
-            if (tilemap.ray(guardPoint, playerPoint, hitPoint))
+            if (Math.abs(FlxMath.distanceBetween(curGuard, player)) <= curGuard.seeDistance)
             {
-                FlxG.log.add("Player Seen!!!!");
+                var hitPoint = new FlxPoint();
+                if (tilemap.ray(guardPoint, playerPoint, hitPoint))
+                {
+                    curGuard.seesPlayer = true;
+                }
+                else {
+                    curGuard.seesPlayer = false;
+                }
             }
             else {
-                FlxG.log.add("Can't see player");
+                curGuard.seesPlayer = false;
             }
 
-            if (curGuard.path.finished || !curGuard.path.active)
+            if (curGuard.canMove || curGuard.seesPlayer)
             {
-                var path = tilemap.findPath(guardPoint, playerPoint);
+                var path:Array<FlxPoint> = null;
+                if (curGuard.seesPlayer)
+                {
+                    path = tilemap.findPath(guardPoint, playerPoint);
+                }
+                else
+                {
+                    var tryPoint = FlxAngle.getCartesianCoords(curGuard.seeDistance, FlxG.random.float(0, 360));
+                    tryPoint.addPoint(guardPoint);
+                    if (tilemap.ray(guardPoint, tryPoint))
+                    {
+                        path = tilemap.findPath(guardPoint, tryPoint);
+                    }
+                }
+
                 if (path == null)
                 {
                     FlxG.log.add("path failed");
                 }
                 else {
                     FlxG.log.add("Startign path");
-                    curGuard.path.start(path, 50);
+                    curGuard.move(path);
                 }
             }
         }
--- source/ent/Guard.hx
+++ source/ent/Guard.hx
@@ -1,18 +1,32 @@
 package ent;
 
+import flixel.math.FlxPoint;
+import flixel.util.FlxTimer;
+import flixel.math.FlxMath;
 import flixel.FlxG;
 import flixel.util.FlxPath;
 import flixel.addons.display.FlxExtendedSprite;
 
 class Guard extends FlxExtendedSprite {
 
-    private var speed:Int = 8;
+    private var moveSpeed:Int = 15;
+    private var runSpeed:Int = 35;
     public var seesPlayer = false;
+    public var seeDistance:Int = 75;
+    public var canMove = true;
+
+    private var moveTimer:FlxTimer;
+    private var seePlayerTimer:FlxTimer;
+
+    private var moveWait:Float = 2.5;
+    private var moveWaitOffset:Float = 2;
 
     public function new(X:Float, Y:Float) {
         super(X, Y);
 
         path = new FlxPath();
+        moveTimer = new FlxTimer();
+        seePlayerTimer = new FlxTimer();
 
         loadGraphic("assets/images/guard.png", true, 8, 8);
 
@@ -20,6 +34,10 @@ class Guard extends FlxExtendedSprite {
         animation.add("idle", [0]);
     }
 
+    public function move(nodes:Array<FlxPoint>):Void {
+        path.start(nodes, seesPlayer ? runSpeed : moveSpeed);
+        canMove = false;
+    }
 
     public override function update(elapsed:Float):Void {
         super.update(elapsed);
@@ -33,6 +51,21 @@ class Guard extends FlxExtendedSprite {
             animation.play("idle");
         }
 
+        if (path.finished && (canMove = false || !moveTimer.active))
+        {
+            if (seesPlayer)
+            {
+                canMove = true;
+            }
+            else
+            {
+                moveTimer.start(FlxG.random.float(0, moveWaitOffset) + moveWait, resetMove);
+            }
+        }
         FlxG.watch.addQuick("GuardPath", path.nodes);
+    }
+
+    private function resetMove(t:FlxTimer):Void {
+        canMove = true;
     }
 }
